dfe1e40568532ce012ce23881cb8e567a278b7c7
LucasGGamerM <lucassggabriel@gmail.com>
2023-06-14 20:42:42 -0300

    fix(instance-about): fix menu item icons being not colored correctly

--- mastodon/src/main/java/org/joinmastodon/android/fragments/InstanceInfoFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/InstanceInfoFragment.java
@@ -334,6 +334,11 @@ public class InstanceInfoFragment extends LoaderFragment {
 	}
 
 	@Override
+	protected boolean wantsToolbarMenuIconsTinted() {
+		return false;
+	}
+
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item){
 		int id=item.getItemId();
 		if(id==R.id.share){
