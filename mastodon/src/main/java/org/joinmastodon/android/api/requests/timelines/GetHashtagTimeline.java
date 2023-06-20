27785e38010d3231d7dfcfeed3c8b2729f21643a
FineFindus <FineFindus@proton.me>
2023-06-20 08:53:34 +0200

    feat(api/hashtag): add any, all, and none parameter

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/timelines/GetHashtagTimeline.java
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/timelines/GetHashtagTimeline.java
@@ -2,6 +2,8 @@ package org.joinmastodon.android.api.requests.timelines;
 
 import com.google.gson.reflect.TypeToken;
 
+import android.text.TextUtils;
+
 import org.joinmastodon.android.GlobalUserPreferences;
 import org.joinmastodon.android.api.MastodonAPIRequest;
 import org.joinmastodon.android.model.Status;
@@ -9,6 +11,22 @@ import org.joinmastodon.android.model.Status;
 import java.util.List;
 
 public class GetHashtagTimeline extends MastodonAPIRequest<List<Status>>{
+	public GetHashtagTimeline(String hashtag, String maxID, String minID, int limit, String[] containsAny, String[] containsAll, String[] containsNone){
+		super(HttpMethod.GET, "/timelines/tag/"+hashtag, new TypeToken<>(){});
+		if(maxID!=null)
+			addQueryParameter("max_id", maxID);
+		if(minID!=null)
+			addQueryParameter("min_id", minID);
+		if(limit>0)
+			addQueryParameter("limit", ""+limit);
+		if(containsAny!=null && containsAny.length!=0)
+			addQueryParameter("any[]", "[" + TextUtils.join(",", containsAny) + "]");
+		if(containsAll!=null && containsAll.length!=0)
+			addQueryParameter("all[]", "[" + TextUtils.join(",", containsAll) + "]");
+		if(containsNone!=null && containsNone.length!=0)
+			addQueryParameter("none[]", "[" + TextUtils.join(",", containsNone) + "]");
+	}
+
 	public GetHashtagTimeline(String hashtag, String maxID, String minID, int limit){
 		super(HttpMethod.GET, "/timelines/tag/"+hashtag, new TypeToken<>(){});
 		if(maxID!=null)
