4e53aa076ae3382fa48dc7b9bf4e93066478b511
Grishka <git@grishka.me>
2023-06-20 22:54:09 +0300

    Fix #604

--- mastodon/src/main/java/org/joinmastodon/android/ui/displayitems/StatusDisplayItem.java
+++ mastodon/src/main/java/org/joinmastodon/android/ui/displayitems/StatusDisplayItem.java
@@ -276,7 +276,7 @@ public abstract class StatusDisplayItem{
 		if(statusForContent.card!=null && statusForContent.mediaAttachments.isEmpty()){
 			contentItems.add(new LinkCardStatusDisplayItem(parentID, fragment, statusForContent));
 		}
-		if (!TextUtils.isEmpty(statusForContent.spoilerText) && statusForContent.spoilerRevealed) {
+		if(contentItems!=items && statusForContent.spoilerRevealed){
 			items.addAll(contentItems);
 		}
 		if((flags & FLAG_NO_FOOTER)==0){
