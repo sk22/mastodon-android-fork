cbacfc8a1c2438645cbd25411c462f46e3fefbc6
Jacocococo <jacocoplays@gmail.com>
2023-11-12 21:00:32 +0100

    Only remove quote-inline with RE:

--- mastodon/src/main/java/org/joinmastodon/android/ui/displayitems/StatusDisplayItem.java
+++ mastodon/src/main/java/org/joinmastodon/android/ui/displayitems/StatusDisplayItem.java
@@ -247,7 +247,7 @@ public abstract class StatusDisplayItem{
 		}
 
 		if(statusForContent.quote!=null) {
-			int quoteInlineIndex=statusForContent.content.indexOf("<span class=\"quote-inline\">");
+			int quoteInlineIndex=statusForContent.content.lastIndexOf("<span class=\"quote-inline\"><br/><br/>RE:");
 			if (quoteInlineIndex!=-1)
 				statusForContent.content=statusForContent.content.substring(0, quoteInlineIndex);
 		}
